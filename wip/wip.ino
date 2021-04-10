ee9b6fea42d57ae83f3f608728237614553e6dd1
universe1987 <tongli.math@gmail.com>
2021-04-10 00:14:05 -0400

    try signal transmission effect

--- wip/wip.ino
+++ wip/wip.ino
@@ -1,7 +1,7 @@
 #include "LEDCurve.h"
 #include "GenericShape.h"
-#include "WaveEffect0D.h"
-#include "WaveEffect1D.h"
+#include "UniformDynamicEffect.h"
+#include "SignalTransmissionEffect.h"
 
 #include <math.h>
 #include <FastLED.h>
@@ -10,13 +10,14 @@
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2812B
 #define NUM_LEDS    120
-#define BRIGHTNESS  128
+#define BRIGHTNESS  160
 
 using namespace LEDGeometry;
 
 CRGB leds[NUM_LEDS];
-Shape* shape = new GenericShape(NUM_LEDS);
-LEDCurve my_light(leds, shape);
+// create a folded led curve, so the shape has half of the points
+Shape* shape = new GenericShape(NUM_LEDS / 2);
+LEDCurve my_light(leds, shape, true);
 
 void setup() {
   delay(3000); // sanity delay
@@ -25,11 +26,12 @@ void setup() {
 }
 
 void loop() {
-  LightEffect* effect1 = new WaveEffect0D(10);
-  // 30 fps for 60 seconds
+  // 10 fps for 30 seconds, color change every 50 frames
+  LightEffect* effect1 = new UniformDynamicEffect(50);
   my_light.set_effect(effect1, 30, 10);
   delete effect1;
-  LightEffect* effect2 = new WaveEffect1D(10);
+  // 10 fps for 30 seconds, color change every 60 frames
+  LightEffect* effect2 = new SignalTransmissionEffect(60);
   my_light.set_effect(effect2, 30, 10);
   delete effect2;
 }
